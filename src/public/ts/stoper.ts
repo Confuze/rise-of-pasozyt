0287983f86e7d313383ff4fc585cbda603285282
Jan Suchanek <jansuchanek0@gmail.com>
2022-03-11 17:33:43 +0100

    fixed /szansa page

--- src/public/ts/stoper.ts
+++ src/public/ts/stoper.ts
@@ -1,21 +1,24 @@
 const h1 = document.querySelector(".header_title");
-            const timeEl = document.querySelector("#time");
-            let setseconds = 3000;
-            let interval:any = null;
+const timeEl = document.querySelector("#time");
+let setseconds = 3000;
+let interval:any = null;
 
-            const watch = () => {
-                setseconds--;
-                if (setseconds <= -1) { 
-                    return clearInterval(interval!);
-                };
+const watch = () => {
+    if (setseconds <= 0) { 
+        return clearInterval(interval);
+    };
 
-                let secs:Number | String = Math.floor(setseconds / 100);
-                let setsecs:Number | String = setseconds % 100;
+    console.log(setseconds);
 
-                secs = secs < 10 ? "0" + secs : secs;
-                setsecs = setsecs < 10 ? "0" + setsecs : setsecs;
+    setseconds--;
 
-                timeEl!.innerHTML = `${secs}.${setsecs}`;
-            };
+    let secs:Number | String = Math.floor(setseconds / 100);
+    let setsecs:Number | String = setseconds % 100;
 
-            interval = setInterval(watch, 10)
+    secs = secs < 10 ? "0" + secs : secs;
+    setsecs = setsecs < 10 ? "0" + setsecs : setsecs;
+
+    timeEl!.innerHTML = `${secs}.${setsecs}`;
+};
+
+interval = setInterval(watch, 10)
